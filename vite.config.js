0117cc54163e7ae0f9767bad732b2ec09309865d
Amitesh Singh <amiteshsinghh.520@gmail.com>
2026-04-23 16:28:38 +0530

    Response Resolve in Vite code

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,13 @@ export default defineConfig({
     react(),
     babel({ presets: [reactCompilerPreset()] })
   ],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://retailuat.abisaio.com:9001',//dev
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
